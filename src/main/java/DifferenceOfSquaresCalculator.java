c2dbfc5c64268fd85fb9704acba27d368089dd5e
etwal <101393718+etwal@users.noreply.github.com>
2022-10-16 13:28:34 -0700

    ehsanSub

--- src/main/java/DifferenceOfSquaresCalculator.java
+++ src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,30 @@
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int sum = 0;
+        for(int i =1; i<=input; i++){
+            sum += i;
+        }
+
+
+        return sum*sum;
     }
 
     int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int sum = 0;
+        for(int i =1; i<=input; i++){
+            sum += i*i;
+        }
+
+        return sum;
     }
 
     int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int sqSum = computeSquareOfSumTo(input);
+        int sumSq = computeSumOfSquaresTo(input);
+
+        return sqSum - sumSq;
+
     }
 
 }
